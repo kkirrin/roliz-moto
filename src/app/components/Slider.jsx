409855b4da07ec0c5388677180182d67e6707588
Сергей Субботин <zarodiny@yandex.ru>
2025-08-18 14:19:57 +1000

    fix: удаление закомментированных доменов и улучшение структуры компонента Slider

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -16,7 +16,7 @@ import { Loader } from "@/app/components/micro/Loader";
 import { useGetSlidersQuery } from "@/redux/api/pages.api";
 import Link from "next/link";
 
-export const Slider = ({}) => {
+export const Slider = ({ }) => {
   const slides = useStater("slides");
 
   const [selectSlide, setSelectSlide] = useState(0);
@@ -52,46 +52,44 @@ export const Slider = ({}) => {
   return (
     <section className="flex flex-row flex-nowrap relative overflow-hidden my-4 rounded-[17px] bg-[linear-gradient(142deg,rgba(0,0,0,0.39)_0%,rgba(0,0,0,0.00)_100%),lightgray_-2.779px_-40.945px/100.373%_130.723%_no-repeat]">
       {!isLoading && data ? (
-        data.data ? (
+        data?.data ? (
           data.data.map((slide, index) => {
             return (
-              <>
-                <article
-                  style={
-                    index === 0
-                      ? { marginLeft: `-${selectSlide * 100}%` }
-                      : null
-                  }
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
-                  key={`slidekey_${index}`}
-                  className={`${styles.sliderItem}`}
-                >
-                  <div className="flex flex-col gap-2 p-8 md:px-12 md:py-9 xl:px-20 xl:py-16 xl:gap-4 ">
-                    <h2 className="text-[22px] md:text-3xl xl:text-[55px] md:leading-[1] xl:leading-[1.2] font-semibold md:w-[380px] xl:w-[655px] text-shadow-sm">
-                      {slide.attributes.header}
-                    </h2>
-                    <p className="text-base xl:text-2xl">
-                      {slide.attributes.desc}
-                    </p>
+              <article
+                style={
+                  index === 0
+                    ? { marginLeft: `-${selectSlide * 100}%` }
+                    : null
+                }
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                key={`slidekey_${index}`}
+                className={`${styles.sliderItem}`}
+              >
+                <div className="flex flex-col gap-2 p-8 md:px-12 md:py-9 xl:px-20 xl:py-16 xl:gap-4 ">
+                  <h2 className="text-[22px] md:text-3xl xl:text-[55px] md:leading-[1] xl:leading-[1.2] font-semibold md:w-[380px] xl:w-[655px] text-shadow-sm">
+                    {slide.attributes.header}
+                  </h2>
+                  <p className="text-base xl:text-2xl">
+                    {slide.attributes.desc}
+                  </p>
 
-                    <button className="font-semibold bg-yellow-default hover:bg-yellow-dark rounded-lg py-3 px-12 w-fit transition-all">
-                      <Link href={`${slide.attributes.href}`}>
-                        Выбрать в каталоге
-                      </Link>
-                    </button>
-                  </div>
+                  <button className="font-semibold bg-yellow-default hover:bg-yellow-dark rounded-lg py-3 px-12 w-fit transition-all">
+                    <Link href={`${slide.attributes.href}`}>
+                      Выбрать в каталоге
+                    </Link>
+                  </button>
+                </div>
 
-                  <div className={`${styles.sliderBg}`}>
-                    <Image
-                      unoptimized
-                      alt={slide.attributes.alt ?? ''}
-                      src={`${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_URL_API}${slide.attributes.src.data.attributes.url}`}
-                      fill
-                    />
-                  </div>
-                </article>
-              </>
+                <div className={`${styles.sliderBg}`}>
+                  <Image
+                    unoptimized
+                    alt={slide.attributes.alt ?? ''}
+                    src={`${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_URL_API}${slide.attributes.src.data.attributes.url}`}
+                    fill
+                  />
+                </div>
+              </article>
             );
           })
         ) : (
@@ -112,17 +110,17 @@ const SliderController = ({ slides, setSelectSlide = (f) => f }) => {
     <div className="w-full h-7 xl:h-10 absolute bottom-0 flex justify-center">
       {slides
         ? slides.map((slide, index) => {
-            if (index > slides.length - 2) return;
-            return (
-              <div
-                onClick={() => {
-                  setSelectSlide(index);
-                }}
-                key={`dotKey_${index}`}
-                className="w-3 h-3 bg-gray-dark hover:bg-yellow-dark cursor-pointer transition-all duration-300 rounded-full z-[9] hover:scale-[1.2] [&:not(:first-child)]:ml-[10px]"
-              ></div>
-            );
-          })
+          if (index > slides.length - 2) return;
+          return (
+            <div
+              onClick={() => {
+                setSelectSlide(index);
+              }}
+              key={`dotKey_${index}`}
+              className="w-3 h-3 bg-gray-dark hover:bg-yellow-dark cursor-pointer transition-all duration-300 rounded-full z-[9] hover:scale-[1.2] [&:not(:first-child)]:ml-[10px]"
+            ></div>
+          );
+        })
         : null}
     </div>
   );
