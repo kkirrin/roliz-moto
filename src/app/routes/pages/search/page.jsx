edf6b004381882a0f54d244ce335844cf13473d0
Сергей Субботин <zarodiny@yandex.ru>
2025-08-18 12:49:43 +1000

    feat: добавляет логику для получения данных поиска

--- src/app/routes/pages/search/page.jsx
+++ src/app/routes/pages/search/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
-import { Suspense, useState } from 'react';
+import { Suspense, useState, useEffect } from 'react';
 
 export default function SearchResultsPage() {
     return (
@@ -14,6 +14,31 @@ function SearchResultsContent() {
     const [dataList, setDataList] = useState([]);
     const searchParams = useSearchParams();
     const query = searchParams.get('query');
+    console.log('dataList', dataList);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (!query) return;
+
+                let tempText = query
+                let tempLetter = `${tempText[0]}`.toUpperCase()
+                tempText = tempLetter + tempText.slice(1)
+
+                const fetchSearch = await fetch(`${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_SEARCH}?filters[$and][0][stock][$gte]=1&filters[$and][1][title][$startsWith]=${tempText.replaceAll(" ", "")}&sort[0]=title:asc`, {
+                    headers: {
+                        "Authorization": `Bearer ${process.env.NEXT_PUBLIC_JWT_KEY}`
+                    }
+                })
+                const result = await fetchSearch.json();
+                setDataList(result?.data);
+
+            } catch (error) {
+                console.error('Ошибка в работе поиска:', error);
+            }
+        };
+
+        fetchData();
+    }, [query]);
 
     return (
         <section>
